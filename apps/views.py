6a0368e61c1464afabe3fab64c63d4df55942d73
jayson7 <lexxiijoo70@gmail.com>
2023-04-14 08:52:20 +0100

    questions and auth modifications

--- apps/views.py
+++ apps/views.py
@@ -2,10 +2,12 @@
 
 
 from django.shortcuts import render, redirect
-
+from authenticator.models import UserLoginToken
+from .models import *
 # Create your views here.
 
-
+def choose_test(request):
+    pass 
 
 
 # 404 here 
@@ -23,4 +25,31 @@ def question_controller(request):
     
     # controll questions 
     
-    
+
+# question 1
+
+def question1(request):
+    context = {}
+    if request.user.is_authenticated:
+        user = request.user 
+        # loctate user on token 
+        try:
+            token_of_user = UserLoginToken.objects.filter(username=user).first()
+            if token_of_user:
+                # prepare question
+                question1 = Questions.object.filter(pk = 1)
+                print = question1.question 
+                question_view = question1.question 
+                
+                # prepare answers 
+                answers = Answer.objects.filter(question=question_view)
+                print(answers)
+                context['answers'] = answers 
+                # send question and answer to view
+                context['question'] = question1
+                return render(request, '', context)
+                
+        except:
+            return redirect('login')     
+    else:
+        return redirect('login')
