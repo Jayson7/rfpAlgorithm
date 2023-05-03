e2ab62b00bcd4dca6cffc5ca0007ec300425c6e0
jayson7 <lexxiijoo70.com>
2023-05-03 14:04:01 +0100

    before

--- rfp_main/urls.py
+++ rfp_main/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authenticator.urls')),
     path('', include('apps.urls')),
-]
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
