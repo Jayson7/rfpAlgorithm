40ee1e61871b4cdc9ce571db206a886a04f1647c
jayson7 <lexxiijoo70.com>
2023-05-03 13:45:07 +0100

    before

--- rfp_main/settings.py
+++ rfp_main/settings.py
@@ -124,13 +124,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+import os
+# static file and static configuration
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+    BASE_DIR / 'static'
+] 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-STATIC_ROOT = "/static/"
+# media files (images, videos, etc.)
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
