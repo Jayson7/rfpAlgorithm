2ed02707b26a06d15a1ed2e8cd8e07449ff0fc6d
jayson7 <lexxiijoo70@gmail.com>
2023-05-27 06:44:06 +0100

    test

--- rfp_main/settings.py
+++ rfp_main/settings.py
@@ -18,10 +18,10 @@ SECRET_KEY = 'django-insecure-(((#46fb3s7x@w7&85f@vdhovj085du@ue0%q9@*0b8=!8oqiv
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'rfpalgporithm.com', 'www.rfpalgorithm.com' ]
-ALLOWED_HOSTS = [ ]
+ALLOWED_HOSTS = ['web-production-46a3.up.railway.app']
 
 
 # Application definition
